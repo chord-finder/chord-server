7108313bb9a23ad013d0f48758dd0b62ede4eea5
Brandon Cook <bcook2289@gmail.com>
2024-08-13 19:31:10 +0900

    feat: completed prisma migrations

--- prisma/migrations/20240812014721_init/migration.sql
+++ prisma/migrations/20240812014721_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE "Chords" (
+CREATE TABLE "chords" (
     "id" SERIAL NOT NULL,
     "strings" VARCHAR(255) NOT NULL,
     "fingering" VARCHAR(255) NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE "Chords" (
     "voicingID" INTEGER NOT NULL,
     "tones" VARCHAR(255) NOT NULL,
 
-    CONSTRAINT "Chords_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "chords_pkey" PRIMARY KEY ("id")
 );
